bf0cf01eaa34fc759d9a348ad7b694b6624df873
kordian127 <kordian126@gmail.com>
2020-12-20 18:52:32 +0100

    Update FrogRiverOne.cpp
    done

--- FrogRiverOne.cpp
+++ FrogRiverOne.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main(){
-int A[]={1,3,1,4,2,3,5,4};
+int A[]={1,3,1,4,2,3,2,4,5};
 int x=5;
 int * indx;
 vector<int> vec;
@@ -14,10 +14,7 @@ for(int i=1; i<=x;i++){
     indx = find(A,A+sizeof(A)/sizeof(A[0]),i);
     vec.push_back(distance(A,indx));
 }
-//sort(vec.begin(),vec.end());
-for(int j=0;j<sizeof(A)/sizeof(A[0]);j++){
-cout<<vec[j]+1<<endl;
-
-}
+sort(vec.begin(),vec.end());
+cout<< "time: " << vec.back()+1 <<endl;
 return 0;
 }
